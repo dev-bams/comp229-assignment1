f4c9967e9ba1c8f6559b96a24751c4f84e3f4bd4
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-05-14 19:59:25 -0400

    added pages

--- src/pages/about-page/AboutContent.tsx
+++ src/pages/about-page/AboutContent.tsx
@@ -1,17 +1,11 @@
 import AboutDetails from "./AboutDetails";
-import khaleed from "../../../src/assets/images/khaleed.png";
 import SkillGroup from "./SkillGroup";
 function AboutContent() {
   return (
     <div className="flex h-full items-center flex-col">
       <div className="flex gap-10 justify-center items-center flex-wrap">
-        <div className="basis-[19rem] grow-[1] flex items-center justify-center">
-          <img src={khaleed} className="max-w-[570px]" />
-        </div>
-        <div className="basis-96 grow-[1] flex flex-col gap-4">
-          <AboutDetails />
-          <SkillGroup />
-        </div>
+        <AboutDetails />
+        <SkillGroup />
       </div>
     </div>
   );
