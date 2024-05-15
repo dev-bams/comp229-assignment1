4345df09e6e5cff5c0281b45358699d30ff81312
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-05-14 20:35:13 -0400

    added hamburger button

--- src/components/navbar/HamburgerButton.tsx
+++ src/components/navbar/HamburgerButton.tsx
@@ -5,12 +5,27 @@ function HamburgerButton() {
         width="48px"
         height="48px"
         viewBox="0 0 24 24"
-        fill="none"
+        fill=""
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path d="M4 18L20 18" strokeWidth="2" strokeLinecap="round" />
-        <path d="M4 12L20 12" strokeWidth="2" strokeLinecap="round" />
-        <path d="M4 6L20 6" strokeWidth="2" strokeLinecap="round" />
+        <path
+          d="M4 18L20 18"
+          stroke="#fff"
+          strokeWidth="2"
+          strokeLinecap="round"
+        />
+        <path
+          d="M4 12L20 12"
+          stroke="#fff"
+          strokeWidth="2"
+          strokeLinecap="round"
+        />
+        <path
+          d="M4 6L20 6"
+          stroke="#fff"
+          strokeWidth="2"
+          strokeLinecap="round"
+        />
       </svg>
     </div>
   );
